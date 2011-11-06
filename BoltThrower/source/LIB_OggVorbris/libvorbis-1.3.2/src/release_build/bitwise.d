986c8b93bfac3428c162cd6647da0119cc097774
Paul Overy <titmouse001@gmail.com>
2011-11-06 19:50:41 +0000

    Add compilable ogg vorbris lib to project (this one is NOT integer based)

---
+++ BoltThrower/source/LIB_OggVorbris/libvorbis-1.3.2/src/release_build/bitwise.d
@@ -0,0 +1,11 @@
+bitwise.o: \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/bitwise.c \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/ogg.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/os_types.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/config_types.h
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/ogg.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/os_types.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/config_types.h:
---
+++ BoltThrower/source/LIB_OggVorbris/libvorbis-1.3.2/src/release_build/lpc.d
@@ -0,0 +1,29 @@
+lpc.o: \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/lpc.c \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/os.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/os_types.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/config_types.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/misc.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/vorbis/codec.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/ogg.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/smallft.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/lpc.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/scales.h
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/os.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/os_types.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/config_types.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/misc.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/vorbis/codec.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/../include/ogg/ogg.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/smallft.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/lpc.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/BoltThrower/source/OggVorbris/libvorbis-1.3.2/src/scales.h:
