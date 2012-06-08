be4c8b92305846a82f6188180920f0f701f9a39f
Paul Overy <titmouse001@gmail.com>
2012-06-08 23:16:10 +0100

    new libogc-src-1.8.11 - with sound tweak

---
+++ Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMOD/build/mwav.d
@@ -0,0 +1,17 @@
+mwav.o: \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/mwav.c \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/mikmod_internals.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/virtualfile.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/mikmod_build.h \
+ c:/devkitPro/libogc/include/gctypes.h \
+ c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/virtualfile.h
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/mikmod_internals.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/virtualfile.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/mikmod_build.h:
+
+c:/devkitPro/libogc/include/gctypes.h:
+
+c:/Projects/BoltThrowerProject.wii/wii-bolt-thrower/Extra_libs_needed.libs/Extra_libs_needed.Source/Source/GRRMod_needed_to_comiple/GRRMOD/GRRMOD/mikmod/playercode/../include/virtualfile.h:
